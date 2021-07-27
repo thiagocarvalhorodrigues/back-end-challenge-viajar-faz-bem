cbf503f16822d8e764768b8f95b2c13feeaf12f2
Thiago Rodrigues <controlrpa@outlook.com>
2021-07-26 23:31:03 -0300

    Send Heroku, newbuild11

--- viajarfazbem/viajarfazbem/settings.py
+++ viajarfazbem/viajarfazbem/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-c)d4_4=l__=$-o#w21uh!b8=09qs-kf6$&=&8l2k2qi3!)u$@(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
